52d4259755ec6fee8867a2e0835b3128c5f64e6a
Ryan Baker <rbaker@xtier.com>
2024-10-25 11:58:20 -0400

    added vertical grid, need to refactor spaghetti code

--- corenew.js
+++ corenew.js
@@ -1,9 +1,14 @@
 
-var scale = 5;
-
-function resizeCanvas()
+function clearCanvas(canvasID)
 {
-    const canvas = document.getElementById("plotCanvas");
+    const canvas = document.getElementById(canvasID);
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function resizeCanvas(canvasID)
+{
+    const canvas = document.getElementById(canvasID);
     const ctx = canvas.getContext("2d");
 
     const width = window.innerWidth;
@@ -13,16 +18,13 @@ function resizeCanvas()
     canvas.height = height * window.devicePixelRatio;
 
     ctx.scale(window,devicePixelRatio, window.devicePixelRatio);
-
-    plotFunction();
 }
 
-function drawAxes()
+function drawGrid(canvasID, yMax)
 {
-    const canvas = document.getElementById("plotCanvas");
+    const canvas = document.getElementById(canvasID);
     const ctx = canvas.getContext("2d");
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
 
     ctx.moveTo(0, canvas.height / 2);
@@ -34,57 +36,55 @@ function drawAxes()
     ctx.lineWidth = 2;
     ctx.stroke();
 
-    const interval = Math.pow(10, Math.floor(Math.log10(scale) - 0.2));
-    const yMax = scale;
+    const interval = Math.pow(10, Math.floor(Math.log10(yMax) - 0.2));
     const aspect = canvas.width / canvas.height;
-    const xMax = scale * aspect;
-
-    for (let y = -interval * 16; y <= interval * 16; y += interval)
+    const xMax = yMax * aspect;
+    
+    for (let i = -Math.floor(10 * aspect); i <= Math.floor(10 * aspect); ++i)
     {
-        if (y > yMax || y < -yMax || Math.abs(y) * 2 < interval)
+        if (i == 0)
             continue;
-        const drawY = (canvas.height / 2) - (y * canvas.height) / (scale * 2);
-        ctx.moveTo(0, drawY);
-        ctx.lineTo(canvas.width, drawY);
-    }
 
-    for (let x = -interval * Math.floor(16 * aspect); x <= interval * Math.floor(16 * aspect); x += interval)
-    {
-        if (x > xMax || x < -xMax || Math.abs(x) * 2 < interval)
-            continue;
-        const drawX = (canvas.width / 2) + (x * canvas.width) / (scale * aspect * 2);
+        const drawX = (canvas.width / 2) + (i * interval * canvas.width) / (xMax * 2);
         ctx.moveTo(drawX, 0);
         ctx.lineTo(drawX, canvas.height);
+
+        if (Math.abs(i) * interval <= yMax)
+        {
+            const drawY = (canvas.height / 2) - (i * interval * canvas.width) / (yMax * 2);
+            ctx.moveTo(0, drawY);
+            ctx.lineTo(canvas.width, drawY);
+        }
     }
 
     ctx.strokeStyle = "grey";
-    ctx.lineWidth = 1;
+    ctx.lineWidth = 0.8;
     ctx.stroke();
 }
 
-function plotFunction()
+function plotFunction(canvasID, funcInput)
 {
-    const canvas = document.getElementById("plotCanvas");
+    const canvas = document.getElementById(canvasID);
+    const func = document.getElementById(funcInput).value;
     const ctx = canvas.getContext("2d");
-    const inputFx = document.getElementById("functionInput").value;
 
-    drawAxes();
     ctx.beginPath();
-
-    const nPoints = 10000;
+    
+    const nPoint = 1000;
     const aspect = canvas.width / canvas.height;
     const tMin = -aspect * scale;
     const yMax = scale * (1 + 10 / canvas.height);
     let yPrev = 0;
 
-    for (let i = 0; i <= nPoints; i++)
+    for (let i = 0; i <= nPoints; ++i)
     {
         const t = tMin + 2 * scale * aspect * i / nPoints;
-        const y = Math.min(Math.max(eval(inputFx), -yMax), yMax);
+        const y = Math.min(Math.max(eval(func), -yMax), yMax);
+
         const drawX =  (canvas.width / 2) + (t *  canvas.width) / (scale * aspect * 2);
         const drawY = (canvas.height / 2) - (y * canvas.height) / (scale * 2);
         
-        if (Math.abs(y) == yMax && Math.abs(yPrev) == yMax)
+        if ((Math.abs(y) == yMax && Math.abs(yPrev) == yMax) || i == 0)
             ctx.moveTo(drawX, drawY);
         else
             ctx.lineTo(drawX, drawY);
@@ -97,9 +97,9 @@ function plotFunction()
     ctx.stroke();
 }
 
-function zoom(event)
+function zoom(event, canvasInfo)
 {
-    const canvas = document.getElementById("plotCanvas");
+    const canvas = document.getElementById(canvasID);
     const ctx = canvas.getContext("2d");
 
     event.preventDefault();
@@ -109,11 +109,9 @@ function zoom(event)
     else
         scale /= Math.max(1 - event.deltaY / 100);
 
-    plotFunction();
+    clearCanvas(canvasID);
+    drawGrid(canvasID);
+    plotFunction(canvasID,
+
+    return scale;
 }
-
-document.getElementById("plotCanvas").addEventListener("wheel", zoom);
-
-window.addEventListener("resize", resizeCanvas);
-
-resizeCanvas();

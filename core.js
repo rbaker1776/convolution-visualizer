682f72a561e928fd834ddd880a8896f425b461d2
Ryan Baker <rbaker@xtier.com>
2024-10-27 15:55:59 -0400

    Wrote plotter class

--- core.js
+++ core.js
@@ -1,154 +1,150 @@
 
 
-function drawAxes()
+class Plotter
 {
-    plotCtx.beginPath();
-
-    plotCtx.moveTo(0, plotCanvas.height / 2);
-    plotCtx.lineTo(plotCanvas.width, plotCanvas.height / 2);
-    plotCtx.moveTo(plotCanvas.width / 2, 0);
-    plotCtx.lineTo(plotCanvas.width / 2, plotCanvas.height);
-
-    plotCtx.strokeStyle = "white";
-    plotCtx.lineWidth = 2;
-    plotCtx.stroke();
-}
-
-function drawGrid()
-{
-    plotCtx.beginPath();
-   
-    const interval = Math.pow(10, Math.floor(Math.log10(plotScale)));
-
-    for (let i = 1; i <= Math.floor(plotScale * plotAspect / interval); i++)
+    constructor(canvas, updateFunc)
     {
-        const drawXR = (plotCanvas.width / 2) + (i * interval) * (plotCanvas.height / (2 * plotScale));
-        const drawXL = (plotCanvas.width / 2) - (i * interval) * (plotCanvas.height / (2 * plotScale));
-        plotCtx.moveTo(drawXR, 0);
-        plotCtx.lineTo(drawXR, plotCanvas.height);
-        plotCtx.moveTo(drawXL, 0);
-        plotCtx.lineTo(drawXL, plotCanvas.height);
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        this.scale = 5;
+        this.aspect = canvas.width / canvas.height;
+        this.update = updateFunc;
+
+        this.resize = this.resize.bind(this);
+        this.zoom = this.zoom.bind(this);
+        this.callUpdate = this.callUpdate.bind(this);
     }
 
-    for (let i = 1; i <= Math.floor(plotScale / interval); i++)
+    drawAxes()
     {
-        const drawYT = (plotCanvas.height / 2) + (i * interval) * (plotCanvas.height / (2 * plotScale));
-        const drawYB = (plotCanvas.height / 2) - (i * interval) * (plotCanvas.height / (2 * plotScale));
-        plotCtx.moveTo(0, drawYT);
-        plotCtx.lineTo(plotCanvas.width, drawYT);
-        plotCtx.moveTo(0, drawYB);
-        plotCtx.lineTo(plotCanvas.width, drawYB);
+        this.ctx.beginPath();
+
+        this.ctx.moveTo(0, this.canvas.height / 2);
+        this.ctx.lineTo(this.canvas.width, this.canvas.height / 2);
+        this.ctx.moveTo(this.canvas.width / 2, 0);
+        this.ctx.lineTo(this.canvas.width / 2, this.canvas.height);
+
+        this.ctx.strokeStyle = "white";
+        this.ctx.lineWidth = 2;
+        this.ctx.stroke();
     }
 
-    plotCtx.strokeStyle = "grey";
-    plotCtx.lineWidth = 1;
-    plotCtx.stroke();
-}
-
-function plotFunction(func, color)
-{
-    let yPrev = (plotCanvas.height / 2) * (1 - func[0] / plotScale);
-    plotCtx.beginPath();
-    plotCtx.moveTo(-1, yPrev);
-
-    for (let i = 0; i < func.length; ++i)
+    drawGrid()
     {
-        const drawX = i / (func.length / plotCanvas.width);
-        const drawY = Math.min(Math.max(
-            ((plotCanvas.height / 2) * (1 - func[i] / plotScale))
-        , -1), plotCanvas.height + 1);
+        this.ctx.beginPath();
+       
+        const interval = Math.pow(10, Math.floor(Math.log10(this.scale)));
 
-        if (
-            (drawY == plotCanvas.height + 1 || drawY == -1)
-         && (yPrev == plotCanvas.height + 1 || yPrev == -1)
-        )
-            plotCtx.moveTo(drawX, drawY);
+        for (let i = 1; i <= Math.floor(this.scale * this.aspect / interval); i++)
+        {
+            const drawXR = (this.canvas.width / 2) + (i * interval) * (this.canvas.height / (2 * this.scale));
+            const drawXL = (this.canvas.width / 2) - (i * interval) * (this.canvas.height / (2 * this.scale));
+            this.ctx.moveTo(drawXR, 0);
+            this.ctx.lineTo(drawXR, this.canvas.height);
+            this.ctx.moveTo(drawXL, 0);
+            this.ctx.lineTo(drawXL, this.canvas.height);
+        }
+
+        for (let i = 1; i <= Math.floor(this.scale / interval); i++)
+        {
+            const drawYT = (this.canvas.height / 2) + (i * interval) * (this.canvas.height / (2 * this.scale));
+            const drawYB = (this.canvas.height / 2) - (i * interval) * (this.canvas.height / (2 * this.scale));
+            this.ctx.moveTo(0, drawYT);
+            this.ctx.lineTo(this.canvas.width, drawYT);
+            this.ctx.moveTo(0, drawYB);
+            this.ctx.lineTo(this.canvas.width, drawYB);
+        }
+
+        this.ctx.strokeStyle = "grey";
+        this.ctx.lineWidth = 1;
+        this.ctx.stroke();
+    }
+
+    plotFunction(func, color)
+    {
+        let yPrev = (this.canvas.height / 2) * (1 - func[0] / this.scale);
+        this.ctx.beginPath();
+        this.ctx.moveTo(-1, yPrev);
+
+        for (let i = 0; i < func.length; ++i)
+        {
+            const drawX = i / (func.length / this.canvas.width);
+            const drawY = Math.min(Math.max(
+                ((this.canvas.height / 2) * (1 - func[i] / this.scale))
+            , -1), this.canvas.height + 1);
+
+            if (
+                (drawY == this.canvas.height + 1 || drawY == -1)
+             && (yPrev == this.canvas.height + 1 || yPrev == -1)
+            )
+                this.ctx.moveTo(drawX, drawY);
+            else
+                this.ctx.lineTo(drawX, drawY);
+
+            yPrev = drawY;
+        }
+
+        this.ctx.strokeStyle = color;
+        this.ctx.lineWidth = 5;
+        this.ctx.stroke();
+    }
+
+    callUpdate()
+    {
+        return this.update.call(this);
+    }
+
+    resize()
+    {
+        const width = window.innerWidth;
+        const height = 400;
+
+        this.canvas.width = width * window.devicePixelRatio;
+        this.canvas.height = height * window.devicePixelRatio;
+        this.aspect = width / height;
+
+        this.ctx.scale(window, window.devicePixelRatio, window.devicePixelRatio);
+
+        this.callUpdate();
+    }
+
+    zoom(event)
+    {
+        event.preventDefault();
+
+        if (event.deltaY > 0)
+            this.scale *= Math.max(1 + event.deltaY / 100);
         else
-            plotCtx.lineTo(drawX, drawY);
+            this.scale /= Math.max(1 - event.deltaY / 100);
 
-        yPrev = drawY;
+        this.callUpdate();
     }
-
-    plotCtx.strokeStyle = color;
-    plotCtx.lineWidth = 5;
-    plotCtx.stroke();
 }
 
-function evaluate(func)
+function evaluate(func, tMin, tMax, deltaT)
 {
-    const tMin = -plotAspect * plotScale;
-    const deltaT = -2 * tMin / plotCanvas.width;
-    func = parse(func.replace(/\bt\b/g, "(t * deltaT + tMin)"));
+    func = func.replace(/\bt\b/g, "(t * deltaT + tMin)");
+    func = parse(func);
 
     return Array.from(
-        { length: Math.ceil((-2 * tMin) / deltaT) },
+        { length: Math.ceil((tMax - tMin) / deltaT) },
         (_, t) => eval(func)
     );
 }
 
-function convolve(ft, gt)
+function convolve(ft, gt, deltaT)
 {
-    const tMin = -plotAspect * plotScale;
-    const deltaT = -2 * plotScale * plotAspect / plotCanvas.width;
-
     let convolution = Array.from(
         { length: ft.length + gt.length - 1 },
         (_, t) => {
             let sum = 0;
             for (let T = Math.max(0, t - (ft.length - 1)); T < Math.min(t, ft.length - 1); T++)
-                sum -= ft[t - T] * gt[T] * deltaT;
+                sum += ft[t - T] * gt[T] * deltaT;
             return sum;
         }
     ).slice(Math.floor(ft.length / 2), Math.floor(ft.length * 3 / 2)); 
 
     return convolution;
-}
-
-function drawPlotCanvas()
-{
-    const inputFt = parse(document.getElementById("fInput").value);
-    const inputGt = parse(document.getElementById("gInput").value)
-
-    plotCtx.clearRect(0, 0, plotCanvas.width, plotCanvas.height);
-    drawAxes()
-    drawGrid();
-    
-    const ft = evaluate(inputFtField.value);
-    const gt = evaluate(inputGtField.value);
-    const convolution = convolve(ft, gt);
-
-    if (displayGtBox.checked)
-        plotFunction(gt, "orange");
-    if (displayFtBox.checked)
-        plotFunction(ft, "red");
-    if (displayConvolutionBox.checked)
-        plotFunction(convolution, "cyan");
-}
-
-function resizePlotCanvas()
-{
-    const width = window.innerWidth;
-    const height = 600;
-
-    plotCanvas.width = width * window.devicePixelRatio;
-    plotCanvas.height = height * window.devicePixelRatio;
-    plotAspect = width / height;
-
-    plotCtx.scale(window, window.devicePixelRatio, window.devicePixelRatio);
-
-    drawPlotCanvas();
-}
-
-function zoomPlot(event)
-{
-    event.preventDefault();
-
-    if (event.deltaY > 0)
-        plotScale *= Math.max(1 + event.deltaY / 100);
-    else
-        plotScale /= Math.max(1 - event.deltaY / 100);
-
-    drawPlotCanvas();
 }
 
 function dropdownFunc(selection)
@@ -168,22 +164,38 @@ function dropdownFunc(selection)
 
 }
 
+functionPlotter = new Plotter(document.getElementById("plotCanvas"), function() {
+    const inputFt = parse(document.getElementById("fInput").value);
+    const inputGt = parse(document.getElementById("gInput").value)
+
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.drawAxes();
+    this.drawGrid();
+    
+    const tMin = -this.aspect * this.scale;
+    const deltaT = -2 * tMin / this.canvas.width;
+
+    const ft = evaluate(inputFtField.value, tMin, -tMin, deltaT);
+    const gt = evaluate(inputGtField.value, tMin, -tMin, deltaT);
+    const convolution = convolve(ft, gt, deltaT);
+
+    if (displayGtBox.checked)
+        this.plotFunction(gt, "orange");
+    if (displayFtBox.checked)
+        this.plotFunction(ft, "red");
+    if (displayConvolutionBox.checked)
+        this.plotFunction(convolution, "cyan");
+});
+
 const e = Math.exp(1);
 
 function dd(t)
 {
-    const deltaT = plotScale * plotAspect * 2 / plotCanvas.width;
+    const deltaT = 2 * functionPlotter.aspect * functionPlotter.scale / functionPlotter.canvas.width;
     return ((
-        (Math.abs(t + deltaT) >= Math.abs(t))
-     && (Math.abs(t - deltaT) >= Math.abs(t))
+        Math.round(t / deltaT) == 0
     ) ? (1 / deltaT) : 0);
 }
-
-const plotCanvas = document.getElementById("plotCanvas");
-const plotCtx = plotCanvas.getContext("2d");
-
-var plotAspect = plotCanvas.width / plotCanvas.height;
-var plotScale = 5;
 
 const inputFtField = document.getElementById("fInput");
 const inputGtField = document.getElementById("gInput");
@@ -195,42 +207,45 @@ const displayFtBox = document.getElementById("toggleFt");
 const displayGtBox = document.getElementById("toggleGt");
 const displayConvolutionBox = document.getElementById("toggleConvolution");
 
-plotCanvas.addEventListener("wheel", zoomPlot);
+functionPlotter.canvas.addEventListener("wheel", functionPlotter.zoom);
 
-window.addEventListener("resize", resizePlotCanvas);
+window.addEventListener("resize", function() {
+    functionPlotter.resize();
+});
 
 inputFtField.addEventListener("keydown", function(event) {
     optionsFt.value = "default"; 
-    setTimeout(drawPlotCanvas, 100);
+    setTimeout(functionPlotter.callUpdate, 100);
 });
 
 inputGtField.addEventListener("keydown", function(event) {
     optionsGt.value = "default"; 
-    setTimeout(drawPlotCanvas, 100);
+    setTimeout(functionPlotter.callUpdate, 100);
 });
 
 optionsFt.addEventListener("change", function() {
     const selection = optionsFt.value;
     inputFtField.value = dropdownFunc(selection);
-    drawPlotCanvas();
+    functionPlotter.callUpdate();
 });
 
 optionsGt.addEventListener("change", function() {
     const selection = optionsGt.value;
     inputGtField.value = dropdownFunc(selection);
-    drawPlotCanvas();
+    functionPlotter.callUpdate();
 });
 
 displayFtBox.addEventListener("change", (event) => {
-    setTimeout(drawPlotCanvas, 100);
+    setTimeout(functionPlotter.callUpdate, 100);
 });
 
 displayGtBox.addEventListener("change", (event) => {
-    setTimeout(drawPlotCanvas, 100);
+    setTimeout(functionPlotter.callUpdate, 100);
 });
 
 displayConvolutionBox.addEventListener("change", (event) => {
-    setTimeout(drawPlotCanvas, 100);
+    setTimeout(functionPlotter.callUpdate, 100);
 });
 
-resizePlotCanvas();
+
+functionPlotter.resize();

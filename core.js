5e94bde4ef4a7c9d584ad0a057156352fc99cccf
Ryan Baker <rbaker@xtier.com>
2024-11-02 12:59:49 -0400

    Finished plot panning and zooming

--- core.js
+++ core.js
@@ -1,6 +1,4 @@
 
-
-
 const inputFtField = document.getElementById("fInput");
 const inputGtField = document.getElementById("gInput");
 
@@ -13,6 +11,7 @@ class Plotter
         this.resize = this.resize.bind(this);
         this.redraw = this.redraw.bind(this);
         this.zoom = this.zoom.bind(this);
+        this.pan = this.pan.bind(this);
 
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
@@ -24,6 +23,8 @@ class Plotter
         this.isPanning = false;
         this.tMin = -this.scale * this.aspectRatio / 2;
         this.fMax = this.scale / 2;
+        this.prevT = 0;
+        this.prevF = 0;
     }
 
     canvasXtoT(x)
@@ -110,7 +111,6 @@ class Plotter
         this.canvas.height = height * window.devicePixelRatio;
         this.aspectRatio = width / height;
         this.tMin = -p * this.scale * this.aspectRatio;
-        console.log(p);
 
         this.ctx.scale(window, window.devicePixelRatio, window.devicePixelRatio);
         
@@ -124,11 +124,35 @@ class Plotter
         const minScale = 2;
         const maxScale = 20;
 
+        const zoomT = this.canvasXtoT(2 * event.offsetX);
+        const centerT = Math.abs(zoomT) < this.scale * this.aspectRatio / 20 ? 0 : zoomT;
+        const ratioT = (centerT - this.tMin) / this.scale;
+
+        const zoomF = this.canvasYtoF(2 * event.offsetY);
+        const centerF = Math.abs(zoomF) < this.scale / 20 ? 0 : zoomF;
+        const ratioF = (this.fMax - centerF) / this.scale;
+
         this.scale = Math.max(Math.min(
-                         this.scale * (1 + event.deltaY / 200)
-                     , maxScale), minScale);
-        this.tMin = -this.scale * this.aspectRatio / 2;
-        this.fMax = this.scale / 2;
+            this.scale * (1 + event.deltaY / 200)
+        , maxScale), minScale);
+        
+        this.tMin = centerT - ratioT * this.scale;
+        this.fMax = centerF + ratioF * this.scale;
+
+        this.redraw();
+    }
+
+    pan(event)
+    {
+        event.preventDefault();
+
+        if (!this.isPanning)
+            return;
+
+        const t = this.canvasXtoT(2 * event.offsetX)
+        this.tMin += this.prevT - t;
+        const f = this.canvasYtoF(2 * event.offsetY)
+        this.fMax += this.prevF - f;
 
         this.redraw();
     }
@@ -149,6 +173,22 @@ const plotters = [functionPlotter];
 
 plotters.forEach((plotter, idx) => {
     plotter.canvas.addEventListener("wheel", plotter.zoom);
+
+    plotter.canvas.addEventListener("mousedown", (event) => {
+        plotter.isPanning = true;
+        plotter.prevT = plotter.canvasXtoT(2 * event.offsetX);
+        plotter.prevF = plotter.canvasYtoF(2 * event.offsetY);
+    });
+
+    plotter.canvas.addEventListener("mouseup", () => {
+        plotter.isPanning = false;
+    });
+
+    plotter.canvas.addEventListener("mouseleave", () => {
+        plotter.isPanning = false;
+    });
+
+    plotter.canvas.addEventListener("mousemove", plotter.pan);
 
     window.addEventListener("resize", plotter.resize);
     
